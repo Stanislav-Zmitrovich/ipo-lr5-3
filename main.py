46ec1f765ddc5badab24421e7f026303c10bb0f7
stasik <stanislavzmitrovic3@gmail.com>
2024-12-17 21:03:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-# Открываем файл bobr.txt в режиме чтения
-with open('bobr.txt', 'r', encoding='utf-8') as file:
-    # Читаем содержимое файла
-    bobr = file.read()
-
-# Разделяем текст на слова
-words = bobr.split()
-
-# Подсчитываем количество слов
-word_count = len(words)
-
-# Выводим количество слов
-print("Количество слов в файле:", word_count)
+with open("text.txt", 'r') as file:  # Открываем файл, выдав имя file
+    stringg = file.read()  # Получаем строку
+words = stringg.split()  # Разделяем ее на слова, записав в список
+print(f'Количество слов в файле "text.txt" {len(words)}')  # Выдаем сообщение с количеством слов
